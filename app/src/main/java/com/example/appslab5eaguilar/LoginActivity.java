0c3d631acfb8a14626a4cbb9ef444eab675c8233
eliecer <elieaguilar91@gmail.com>
2019-10-15 22:51:58 -0500

    mejora de la pantalla del login

--- app/src/main/java/com/example/appslab5eaguilar/LoginActivity.java
+++ app/src/main/java/com/example/appslab5eaguilar/LoginActivity.java
@@ -3,6 +3,7 @@ package com.example.appslab5eaguilar;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.DisplayMetrics;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -12,5 +13,20 @@ public class LoginActivity extends AppCompatActivity {
         setContentView(R.layout.activity_login);
     }
 
+    //metodo para calcular tamano de pantalla
+    public String tamanoX(){
+        DisplayMetrics tam = new DisplayMetrics();
+        getWindowManager().getDefaultDisplay().getMetrics(tam);
+        int width = tam.widthPixels; // ancho absoluto en pixels
+        int height = tam.heightPixels; // alto absoluto en pixels
 
+        float xDP = tam.xdpi;
+        float yDP = tam.ydpi;
+
+        float ret = (float) (xDP*0.3);
+
+        String r = Float.toString(ret);
+        return  r;
+
+    }
 }
