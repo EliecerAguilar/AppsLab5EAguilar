ea7b7a00181a17996175c51a9e2eb41f0cec94a9
eliecer <elieaguilar91@gmail.com>
2019-10-15 22:57:35 -0500

    func load inicializar controles y conectar con diseno

--- app/src/main/java/com/example/appslab5eaguilar/LoginActivity.java
+++ app/src/main/java/com/example/appslab5eaguilar/LoginActivity.java
@@ -4,13 +4,31 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.util.DisplayMetrics;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class LoginActivity extends AppCompatActivity {
+
+    private EditText edtUsr;
+    private EditText edtPass;
+    //private Button btnLogin;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        this.load();
+
     }
+
+
+    protected void load(){
+        edtUsr = (EditText) findViewById(R.id.ETusr);
+        edtPass = (EditText)findViewById(R.id.ETpass);
+    }
+
+
+
 
 }
